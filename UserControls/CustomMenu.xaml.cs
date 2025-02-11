92bb2bbc2f74dca70bdba82e920e6e8787c89825
Alex <aliclarke.soly@gmail.com>
2025-02-11 08:08:20 +0100

    initial commit

---
+++ UserControls/CustomMenu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditor.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CustomMenu.xaml
+    /// </summary>
+    public partial class CustomMenu : UserControl
+    {
+        public CustomMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Clicked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Cheese");
+        }
+    }
+}
---
+++ UserControls/CustomToolbar.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditor.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CustomToolbar.xaml
+    /// </summary>
+    public partial class CustomToolbar : UserControl
+    {
+        public CustomToolbar()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            for (int i = 8; i < 48; i+=2)
+            {
+                fontSize.Items.Add(i);
+            }
+        }
+    }
+}
