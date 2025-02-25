f114dbb6e7bb7386cdf94d6c74b2b33bdb9e9fd2
Clarke Alex <aliclarke.soly@gmail.com>
2025-02-25 10:07:02 +0100

    Add files via upload

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,51 +8,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using TextEditor.UserControls;
-using System;
 
-namespace TextEditor
+namespace AminekAkarom
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DocumentManager _documentManager;
         public MainWindow()
         {
             InitializeComponent();
-            _documentManager = new DocumentManager(body);
-
-            if (_documentManager.OpenDocument())
-            {
-                status.Text = "Document Loaded";
-            }
-        }
-
-        private void CustomToolbar_SelectionChanged (object sender, SelectionChangedEventArgs e)
-        {
-            if (toolbar.IsSynchronizing) return;
-
-            ComboBox source=e.OriginalSource as ComboBox;
-            if (source == null) return;
-
-            switch (source.Name)
-            {
-                case "fonts":
-                    _documentManager.ApplyToSelection(TextBlock.FontFamilyProperty, source.SelectedItem);
-                    break;
-                case "fontSize":
-                    _documentManager.ApplyToSelection(TextBlock.FontSizeProperty, source.SelectedItem);
-                    break;
-            }
-
-            body.Focus();
-        }
-
-        private void body_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-            toolbar.SynchronizeWith(body.Selection);
         }
     }
 }
